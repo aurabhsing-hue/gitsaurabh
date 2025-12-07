d7ecda81b2bf054a59883ebc8a835689d7bc847d
Saurabh Singh <aurabhsing@gmail.com>
2025-12-07 23:43:39 +0530

    Modified after cloning

--- Launch.java
+++ Launch.java
@@ -2,4 +2,10 @@ class Launch {
 	public static void main(String[] args){
 		System.out.println("Exploring Git & GitHub");
 	}
+	public static void add()
+	{
+		int a = 5;
+		int b = 10;
+		int sum = a + b;
+	}
 }
